73a439cec4a6ef376bd1e4314e2e783adb65eeb2
BhavyaBharathi <bhavya2akkala@gmail.com>
2024-12-12 11:50:18 +0530

    Initial commit

---
+++ bhavyaerp/doctype/bhavyaerp/bhavyaerp.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Bhavya and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Bhavyaerp", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ bhavyaerp/doctype/bhavyaerp/test_bhavyaerp.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Bhavya and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestBhavyaerp(FrappeTestCase):
+	pass
